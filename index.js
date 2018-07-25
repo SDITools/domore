2f5f0d78c6ef9317f3ef2389893bf0767a5f4c99
Micah Wedemeyer <me@micahwedemeyer.com>
2018-07-25 10:22:33 -0400

    First cut of Users class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {DomoClient} = require('domo-sdk');
 const DomoConstants = require('domo-sdk/dist/common/Constants');
+const User = require('./lib/user');
 
 class DomoreClient {
   constructor(clientId, clientSecret, scopes, host = 'api.domo.com') {
@@ -9,10 +10,15 @@ class DomoreClient {
     this.host = host;
 
     this.domoClient = new DomoClient(clientId, clientSecret, scopes, host);
+    this.user = new User(this.domoClient);
   }
 
   get domo() {
     return this.domoClient;
+  }
+
+  get users() {
+    return this.user;
   }
 }
 
